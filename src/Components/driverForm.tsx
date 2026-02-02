29f3ee996f725dea3386fd24c65289c938079577
Oleksii Burdeniuk <alejandroburdenyk@gmail.com>
2026-02-02 19:32:44 +0100

    translation set up finished

--- src/Components/driverForm.tsx
+++ src/Components/driverForm.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useState } from 'react';
 import styles from './DriverForm.module.css';
+import { useTranslations } from 'next-intl';
 
 export default function DriverForm() {
   const [formData, setFormData] = useState({
@@ -10,6 +11,7 @@ export default function DriverForm() {
     city: '',
     consent: false,
   });
+  const t = useTranslations('HomePage');
 
   const handleChange = (e: {
     target: { name: string; value?: string; type?: string; checked?: boolean };
@@ -47,7 +49,7 @@ Phone: ${phoneNumber}
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.mainTitle}>{"t('title')"}</h1>
+      <h1 className={styles.mainTitle}>{t('title')}</h1>
       <p className={styles.subTitle}>Send us email for contact</p>
       <form className={styles.form} onSubmit={handleSubmit}>
         <input
--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -2,11 +2,12 @@ import { Inter } from 'next/font/google';
 
 import './globals.css';
 
-import { hasLocale } from 'next-intl';
+import { hasLocale, NextIntlClientProvider } from 'next-intl';
 import { notFound } from 'next/navigation';
 import { routing } from '@/i18n/routing';
 import Header from '@/Components/header';
 import Footer from '@/Components/footer';
+import { getMessages } from 'next-intl/server';
 
 const inter = Inter({ subsets: ['latin', 'cyrillic'] });
 
@@ -21,18 +22,21 @@ export default async function LocaleLayout({
   children: React.ReactNode;
   params: Promise<{ locale: string }>;
 }) {
-  const { locale } = await params;
-  if (!hasLocale(routing.locales, locale)) {
-    notFound();
-  }
+  // const { locale } = await params;
+  // if (!hasLocale(routing.locales, locale)) {
+  //   notFound();
+  // }
+  // const messages = await getMessages();
   return (
     <html>
       <body className={inter.className}>
-        <div className='flex min-h-screen flex-col'>
-          <Header />
-          <main className='flex-grow'>{children}</main>
-          <Footer />
-        </div>
+        <NextIntlClientProvider>
+          <div className='flex min-h-screen flex-col'>
+            <Header />
+            <main className='flex-grow'>{children}</main>
+            <Footer />
+          </div>
+        </NextIntlClientProvider>
       </body>
     </html>
   );
